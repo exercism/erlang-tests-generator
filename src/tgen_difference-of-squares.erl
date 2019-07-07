f5d3b9006e5f653e4573b5b80b4c11fa948e4f57
Norbert Melzer <nmelzer@nobbz.dev>
2019-07-07 10:57:33 +0200

    update difference-of-squares

--- src/tgen_difference-of-squares.erl
+++ src/tgen_difference-of-squares.erl
@@ -13,10 +13,12 @@ generate_test(N, #{description := Desc, expected := Exp, property := Prop, input
     TestName = tgen:to_test_name(N, Desc),
     Property = tgen:to_property_name(Prop),
 
-    Fn = tgs:simple_fun(TestName, [
-        tgs:call_macro("assertEqual", [
-            tgs:value(Exp),
-            tgs:call_fun("difference_of_squares:" ++ Property, [
-                tgs:value(Number)])])]),
+    Fn = tgs:simple_fun(TestName ++ "_", [
+        erl_syntax:tuple([
+            tgs:string(Desc),
+            tgs:call_macro("_assertEqual", [
+                tgs:value(Exp),
+                tgs:call_fun("difference_of_squares:" ++ Property, [
+                    tgs:value(Number)])])])]),
 
     {ok, Fn, [{Property, ["Number"]}]}.
