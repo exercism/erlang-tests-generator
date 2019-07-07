18f55ac84697ff36728b748ac8fda4c232df4455
Norbert Melzer <nmelzer@nobbz.dev>
2019-07-07 11:00:34 +0200

    update dominoes

--- src/tgen_dominoes.erl
+++ src/tgen_dominoes.erl
@@ -13,14 +13,16 @@ generate_test(N, #{description := Desc, expected := Exp, property := Prop, input
     TestName = tgen:to_test_name(N, Desc),
     Property = tgen:to_property_name(Prop),
 
-    Assert = case Exp of
+    Assert = "_" ++ case Exp of
         true -> "assert";
         false -> "assertNot"
     end,
 
-    Fn = tgs:simple_fun(TestName, [
-        tgs:call_macro(Assert, [
-            tgs:call_fun("dominoes:" ++ Property, [
-                tgs:value([{L, R} || [L, R] <- Dominoes])])])]),
+    Fn = tgs:simple_fun(TestName ++ "_", [
+        erl_syntax:tuple([
+            tgs:string(Desc),
+            tgs:call_macro(Assert, [
+                tgs:call_fun("dominoes:" ++ Property, [
+                    tgs:value([{L, R} || [L, R] <- Dominoes])])])])]),
 
     {ok, Fn, [{Property, ["Dominoes"]}]}.
