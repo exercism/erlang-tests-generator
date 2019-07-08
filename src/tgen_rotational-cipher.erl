2a3a96e284f411b77104238d507e9e9318fc6ea7
Norbert Melzer <nmelzer@nobbz.dev>
2019-07-08 15:26:34 +0200

    update rotational-cipher

--- src/tgen_rotational-cipher.erl
+++ src/tgen_rotational-cipher.erl
@@ -14,14 +14,19 @@ generate_test(N, #{description := Desc, expected := Exp, property := <<"rotate">
     Property1 = tgen:to_property_name(<<"encrypt">>),
     Property2 = tgen:to_property_name(<<"decrypt">>),
 
-    Fn = tgs:simple_fun(TestName, [
-        tgs:call_macro("assertEqual", [
-            tgs:value(binary_to_list(Exp)),
-            tgs:call_fun("rotational_cipher:" ++ Property1, [
-                tgs:value(binary_to_list(String)), tgs:value(Key)])]),
-        tgs:call_macro("assertEqual", [
-            tgs:value(binary_to_list(String)),
-            tgs:call_fun("rotational_cipher:" ++ Property2, [
-                tgs:value(binary_to_list(Exp)), tgs:value(Key)])])]),
+    Fn = tgs:simple_fun(TestName ++ "_", [
+        erl_syntax:list([
+            erl_syntax:tuple([
+                tgs:string([Desc, " (encrypt)"]),
+                tgs:call_macro("_assertEqual", [
+                    tgs:value(binary_to_list(Exp)),
+                    tgs:call_fun("rotational_cipher:" ++ Property1, [
+                        tgs:value(binary_to_list(String)), tgs:value(Key)])])]),
+            erl_syntax:tuple([
+                tgs:string([Desc, " (decrypt)"]),
+                tgs:call_macro("_assertEqual", [
+                    tgs:value(binary_to_list(String)),
+                    tgs:call_fun("rotational_cipher:" ++ Property2, [
+                        tgs:value(binary_to_list(Exp)), tgs:value(Key)])])])])]),
 
     {ok, Fn, [{Property1, ["String", "Key"]}, {Property2, ["String", "Key"]}]}.
