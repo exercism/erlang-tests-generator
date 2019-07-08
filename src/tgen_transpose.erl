3e700c6f1cbba896bfc261debf391a36f54cf487
Norbert Melzer <nmelzer@nobbz.dev>
2019-07-08 22:37:53 +0200

    update transpose

--- src/tgen_transpose.erl
+++ src/tgen_transpose.erl
@@ -13,13 +13,15 @@ generate_test(N, #{description := Desc, expected := Exp, property := Prop, input
     TestName = tgen:to_test_name(N, Desc),
     Property = tgen:to_property_name(Prop),
 
-    Fn = tgs:simple_fun(TestName, [
+    Fn = tgs:simple_fun(TestName ++ "_", [
         tgs:raw("Input="++format_lines(Lines)),
         tgs:raw("Expected="++format_lines(Exp)),
-        tgs:call_macro("assertEqual", [
-            tgs:raw("Expected"),
-            tgs:call_fun("transpose:" ++ Property, [
-                tgs:raw("Input")])])]),
+        erl_syntax:tuple([
+            tgs:string(Desc),
+            tgs:call_macro("_assertEqual", [
+                tgs:raw("Expected"),
+                tgs:call_fun("transpose:" ++ Property, [
+                    tgs:raw("Input")])])])]),
 
     {ok, Fn, [{Property, ["Lines"]}]}.
 
