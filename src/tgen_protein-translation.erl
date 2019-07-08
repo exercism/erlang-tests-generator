50545fa17b10caf52147966f6f57d8761b7cced9
Norbert Melzer <nmelzer@nobbz.dev>
2019-07-08 13:38:53 +0200

    update protein-translation

--- src/tgen_protein-translation.erl
+++ src/tgen_protein-translation.erl
@@ -22,11 +22,13 @@ generate_test(N, #{description := Desc, expected := Exp, property := Prop, input
     TestName = tgen:to_test_name(N, Desc),
     Property = tgen:to_property_name(Prop),
 
-    Fn = tgs:simple_fun(TestName, [
-        tgs:call_macro("assertMatch", [
-            tgs:value(Exp),
-            tgs:call_fun("protein_translation:" ++ Property, [
-                tgs:value(binary_to_list(Strand))])])]),
+    Fn = tgs:simple_fun(TestName ++ "_", [
+        erl_syntax:tuple([
+            tgs:string(Desc),
+            tgs:call_macro("_assertMatch", [
+                tgs:value(Exp),
+                tgs:call_fun("protein_translation:" ++ Property, [
+                    tgs:value(binary_to_list(Strand))])])])]),
 
     {ok, Fn, [{Property, ["Strand"]}]}.
 
