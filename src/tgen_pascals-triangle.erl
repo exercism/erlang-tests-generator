8aeed85d7c8402f9abfaf769ed168033cc33c2a4
Norbert Melzer <nmelzer@nobbz.dev>
2019-07-08 13:29:59 +0200

    update perfect-numbers

--- src/tgen_pascals-triangle.erl
+++ src/tgen_pascals-triangle.erl
@@ -13,10 +13,12 @@ generate_test(N, #{description := Desc, expected := Exp, property := Prop, input
     TestName = tgen:to_test_name(N, Desc),
     Property = tgen:to_property_name(Prop),
 
-    Fn = tgs:simple_fun(TestName, [
-        tgs:call_macro("assertMatch", [
-            tgs:value(Exp),
-            tgs:call_fun("pascals_triangle:" ++ Property, [
-                tgs:value(Count)])])]),
+    Fn = tgs:simple_fun(TestName ++ "_", [
+        erl_syntax:tuple([
+            tgs:string(Desc),
+            tgs:call_macro("_assertMatch", [
+                tgs:value(Exp),
+                tgs:call_fun("pascals_triangle:" ++ Property, [
+                    tgs:value(Count)])])])]),
 
     {ok, Fn, [{Property, ["Count"]}]}.
--- src/tgen_perfect-numbers.erl
+++ src/tgen_perfect-numbers.erl
@@ -13,11 +13,13 @@ generate_test(N, #{description := Desc, expected := #{error := _}, property := P
     TestName = tgen:to_test_name(N, Desc),
     Property = tgen:to_property_name(Prop),
 
-    Fn = tgs:simple_fun(TestName, [
-        tgs:call_macro("assertError", [
-            tgs:raw("_"),
-            tgs:call_fun("perfect_numbers:" ++ Property, [
-                tgs:value(Number)])])]),
+    Fn = tgs:simple_fun(TestName ++ "_", [
+        erl_syntax:tuple([
+            tgs:string(Desc),
+            tgs:call_macro("_assertError", [
+                tgs:raw("_"),
+                tgs:call_fun("perfect_numbers:" ++ Property, [
+                    tgs:value(Number)])])])]),
 
     {ok, Fn, [{Property, ["Number"]}]};
 
@@ -25,10 +27,12 @@ generate_test(N, #{description := Desc, expected := Exp, property := Prop, input
     TestName = tgen:to_test_name(N, Desc),
     Property = tgen:to_property_name(Prop),
 
-    Fn = tgs:simple_fun(TestName, [
-        tgs:call_macro("assertEqual", [
-            tgs:value(binary_to_atom(Exp, latin1)),
-            tgs:call_fun("perfect_numbers:" ++ Property, [
-                tgs:value(Number)])])]),
+    Fn = tgs:simple_fun(TestName ++ "_", [
+        erl_syntax:tuple([
+            tgs:string(Desc),
+            tgs:call_macro("_assertEqual", [
+                tgs:value(binary_to_atom(Exp, latin1)),
+                tgs:call_fun("perfect_numbers:" ++ Property, [
+                    tgs:value(Number)])])])]),
 
     {ok, Fn, [{Property, ["Number"]}]}.
