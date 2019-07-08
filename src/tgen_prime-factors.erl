21ee7faddb99710566f9ebe86b1a60f3b560ea06
Norbert Melzer <nmelzer@nobbz.dev>
2019-07-08 13:35:50 +0200

    update prime_factors

--- src/tgen_prime-factors.erl
+++ src/tgen_prime-factors.erl
@@ -13,11 +13,13 @@ generate_test(N, #{description := Desc, expected := Exp, property := Prop, input
     TestName = tgen:to_test_name(N, Desc),
     Property = tgen:to_property_name(Prop),
 
-    Fn = tgs:simple_fun(TestName, [
-        tgs:call_macro("assertEqual", [
-            tgs:call_fun("lists:sort", [tgs:value(Exp)]),
-            tgs:call_fun("lists:sort", [
-                tgs:call_fun("prime_factors:" ++ Property, [
-                    tgs:value(Value)])])])]),
+    Fn = tgs:simple_fun(TestName ++ "_", [
+        erl_syntax:tuple([
+            tgs:string(Desc),
+            tgs:call_macro("_assertEqual", [
+                tgs:call_fun("lists:sort", [tgs:value(Exp)]),
+                tgs:call_fun("lists:sort", [
+                    tgs:call_fun("prime_factors:" ++ Property, [
+                        tgs:value(Value)])])])])]),
 
     {ok, Fn, [{Property, ["Value"]}]}.
