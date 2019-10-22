41e81b83d36dc5f0ea58cb60d425e962e67d701e
wojtii <wojdna@gmail.com>
2019-10-22 16:49:40 +0200

    Change tgen_collatz-conjecture to assert badarg error instead of tuple

--- src/tgen_collatz-conjecture.erl
+++ src/tgen_collatz-conjecture.erl
@@ -7,17 +7,17 @@
     generate_test/2
 ]).
 
-revision() -> 1.
+revision() -> 2.
 
-generate_test(N, #{description := Desc, expected := #{error := Message}, property := Prop, input := #{number := Num}}) ->
+generate_test(N, #{description := Desc, expected := #{error := _}, property := Prop, input := #{number := Num}}) ->
     TestName = tgen:to_test_name(N, Desc),
     Property = tgen:to_property_name(Prop),
 
     Fn = tgs:simple_fun(TestName ++ "_", [
         erl_syntax:tuple([
             tgs:string(Desc),
-            tgs:call_macro("_assertMatch", [
-                tgs:value({error, binary_to_list(Message)}),
+            tgs:call_macro("_assertError", [
+                tgs:value(badarg),
                 tgs:call_fun("collatz_conjecture:" ++ Property, [
                     tgs:value(Num)])])])]),
 
